9e925b8b5d65cb5ec9f2061bdab9e5368bbcd73a
Aron <aronmn@vgy.se>
2016-09-30 14:55:17 +0200

    övning 1

--- Strängar/Program.cs
+++ Strängar/Program.cs
@@ -10,6 +10,9 @@ namespace Strängar
     {
         static void Main(string[] args)
         {
+            //Övning 1
+            Console.WriteLine("\u0056\u00E4\u0072\u006D\u0064\u00F6\u00AD\u0067\u0079\u006D\u006E\u0061\u0073\u0069\u0075\u006D");
+
         }
     }
 }
